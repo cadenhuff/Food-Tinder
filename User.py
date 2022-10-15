bcbf8a615e6665fb55c61e6d9231432e89557e46
Caden Huffman <you@example.com>
2022-10-14 23:53:44 -0400

    Intialized all vars/ Started working on main function

---
+++ User.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self,location,preference,distance,allergies,min_price,max_price,time):
+        self.location = location
+        
+
--- sample.py
+++ sample.py
@@ -1,4 +1,25 @@
 import pandas as pd
 import numpy as np
+from User import User
 
-print("hello world")
+location = input("Enter a Location!") #IDK how this will work in the code, might need to do address
+preference = input("What kind of food do you like") #Could do a ranking 1-5
+distance
+allergies
+min_price
+max_price
+time =
+
+
+
+
+
+def main():
+    x = User(location,1,2,3,4,5,6)
+    print(x.location)
+
+
+
+
+
+main()
