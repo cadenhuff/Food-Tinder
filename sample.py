07469ecfd44b67c3969ce7ed7b106fe56d2de649
Eric Davis <ericdavis@Erics-MacBook-Air.local>
2022-10-15 11:35:27 -0400

    sample kv file

--- sample.py
+++ sample.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import numpy as np
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
 
-print("hello world")
+class MyApp(App):
+
+    def build(self):
+        return Label(text = "Hello my name is Eric")
+
+if __name__ == "__main__":
+    MyApp().run()
+
