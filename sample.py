ff5263871d837c989f1e6b63b2d840a0708dae49
Caden Huffman <you@example.com>
2022-10-14 23:59:34 -0400

    de

--- sample.py
+++ sample.py
@@ -4,11 +4,11 @@ from User import User
 
 location = input("Enter a Location!") #IDK how this will work in the code, might need to do address
 preference = input("What kind of food do you like") #Could do a ranking 1-5
-distance
-allergies
-min_price
-max_price
-time =
+distance = input("How far are you will to go?")
+allergies = input("Do you have any allergies?")
+min_price = input("")
+max_price = input("")
+time = input("What time are you thinking of going?")
 
 
 
