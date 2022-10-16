0e8760da203e1e25c79b06cb3a2bedd385d65047
Eric Davis <ericdavis@crc-dot1x-nat-10-239-51-130.bu.edu>
2022-10-15 23:06:54 -0400

    animation close to done

--- animation.py
+++ animation.py
@@ -1,32 +1,33 @@
-#import kivy
-#from kivy.app import App
-#from kivy.uix.label import Label
-
-#class MyApp(App):
-
- #   def build(self):
-  #      return Label(text = "Hello my name is Eric")
-
-#if __name__ == "__main__":
- #   MyApp().run()
-
 import requests
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
-from kivy.clock import Clock
-from timeit import time
+from kivy.properties import StringProperty
+import random
 
-Window.size=(300,300)
+Window.size=(700,800)
 Builder.load_file('animation.kv')
-
 
 class MyLayout(Widget):
     pass
 
+restaurants = ['1', '2']
+picdict = {'1':'photo.jpeg', '2':'photo2.jpeg'}
+textdict = {'1':'this is the first restaurant', '2':'this is the second restaurant'}
 
 class LoopApp(App):
+    test_image = StringProperty()
+    test_text = StringProperty()
+
+    def info(self):
+        selection = random.choice(restaurants)
+        restaurants.remove(selection)
+        self.test_image = picdict[selection]
+        self.test_text = textdict[selection]
+
+    
     def build(self):
         return MyLayout()
 
